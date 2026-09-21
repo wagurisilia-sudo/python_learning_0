3dea8ae7b84d44891f7a8b15fc095f6066da90b0
Sakura <sakura@local>
2026-09-21 20:36:52 +0200

    Add automatic web opening(mode:ask)

--- arch_search/main.py
+++ arch_search/main.py
@@ -1,11 +1,31 @@
+import webbrowser
+
+def open_archlinux_website(url):
+ webbrowser.open(url)
+
+def browser_mode_ask():
+ while True:
+  choice = input("Do you want to open this website? (yes/no)\n").lower()
+  if choice == "yes" :
+   
+   return True
+
+  if choice == "no":
+   
+   return False
+    
+  else :
+   
+   continue
+
+
+browser_mode = "ask"
+
 name = input("Hello, what your name?\n")
+
 print("Hello", name, "\n",)
 
-language = input("This is a arch linux website guide, Which language do you need?(English, French, German, Japanese, Chinese)\n").lower()
-
-while True:
- 
- languages = {
+languages = {
   "english": "https://archlinux.org/",
     "french": "https://archlinux.fr/",
     "german": "https://archlinux.de/",
@@ -13,11 +33,35 @@ while True:
     "chinese": "https://www.archlinuxcn.org/",
  } 
 
+language = input("This is a arch linux website guide, Which language do you need?(English, French, German, Japanese, Chinese)\n").lower()
+
+while True:
+
  if language in languages:
-  print("WEB:",languages[language])
-  break
+  print("WEB:",(languages[language]),)
+
+  if browser_mode == "always":
+   open_archlinux_website(languages[language])
+   break
+  
+
+  elif browser_mode == "ask":
+   if browser_mode_ask() :
+    open_archlinux_website(languages[language])
+   break
+   
+
+
+     
+  else :
+   print("[ERROR1002], incorrect mode\n")
+   break
+
+
+
+
  else :
-  language = input("Sorry, please try again (or quit)[ERROR:1001]").lower()
+  language = input("Sorry, please try again (or quit)[ERROR:1001]\n").lower()
   if language == "quit":
    break  
  
